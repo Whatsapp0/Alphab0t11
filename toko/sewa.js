8818b8d635c46610533f998cbf09fcde9b4d6147
Whatsapp0 <97558539+Whatsapp0@users.noreply.github.com>
2022-01-12 04:05:58 +0700

    Update and rename shop/sewa.js to toko/sewa.js

--- toko/sewa.js
+++ toko/sewa.js
@@ -3,21 +3,20 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = ${_minggu}
-⬡ 2 MINGGU = ${__minggu}
-⬡ 3 MINGGU = ${___minggu}
-⬡ 1 BULAN = ${_bulan}
-⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
+⬡ 1 MINGGU = 3K
+⬡ 2 MINGGU = 6K
+⬡ 3 MINGGU = 8K
+⬡ 1 BULAN = 10K
+⬡ PERMANEN = 15K
 
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 15K
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285640099480
 
 
 NOTE : 
