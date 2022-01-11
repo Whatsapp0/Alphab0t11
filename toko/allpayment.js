213ab6351194e8082be062654c6ebbd3c09fd3e2
Whatsapp0 <97558539+Whatsapp0@users.noreply.github.com>
2022-01-12 04:00:24 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

--- toko/allpayment.js
+++ toko/allpayment.js
@@ -1,27 +1,15 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0896 8088 7039
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0896 8088 7039
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0896 8088 7039
 	
-Semuanya atas nama RI****DI
 
 *📲 PULSA*
 
